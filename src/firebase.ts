31c908c41a04f1a0947d512dc6fbaa6812aee0a0
SarahKinner <156657511+SarahKinner@users.noreply.github.com>
2026-05-22 10:22:53 -0600

    Added some stuff and tested the firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -18,4 +18,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+//Testing
+console.log("Firebase connected:", app);
+
+export default app;
